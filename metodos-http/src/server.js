36195f6eb9f784cc1e36b7dce8ffd793c8c5fa26
Luís Marinho <luisdm5309@gmail.com>
2025-07-06 17:05:47 -0300

    Modelagem e organização do front-end

--- metodos-http/src/server.js
+++ metodos-http/src/server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 })

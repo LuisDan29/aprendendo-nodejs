df695695536f19a9a8879cfbee737edda8b63900
Luís Marinho <luisdm5309@gmail.com>
2025-07-07 22:52:10 -0300

    Organização do front-end

--- metodos-http/public/script.js
+++ metodos-http/public/script.js
@@ -1,4 +1,4 @@
-const URL = 'http://localhost:3000';
+const URL = 'http://localhost:8081';
 const GRID = document.getElementById('grid');
 
 async function getPaleta() {
@@ -15,31 +15,43 @@ async function getPaleta() {
 async function showPaleta() {
     getPaleta().then(paleta => {
         paleta.forEach(cor => {
-            const div = document.createElement('div');
-            div.classList.add('cor');
-            div.setAttribute('data-id', cor.id);
-            div.style.backgroundColor = `${cor.hex}`;
-
-            // const p = document.createElement('p');
-            // p.innerText = `${cor.nome}`;
-            // p.style.color = ajustarCor(cor.hex);
-
-            // div.appendChild(p);
-            GRID.appendChild(div);
-        })
+            GRID.appendChild(buildCor(cor));
+        });
     })
 }
 
 showPaleta();
 
-// function ajustarCor(cor) {
-//     const valores = cor.match(/\d+/g).map(Number);
-//     const [r, g, b] = valores;
-//     const luminancia = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+function buildCor(cor) {
+    const div = document.createElement('div');
+    div.classList.add('cor');
+    div.setAttribute('data-id', cor.id);
+    div.style.backgroundColor = cor.hex;
 
-//     if (luminancia < 128) {
-//         return 'var(--color6)';
-//     } else {
-//         return 'var(--color8)';
-//     }
-// }
+    const pNome = document.createElement('p');
+    const pHex = document.createElement('p');
+    pNome.innerText = cor.nome.toUpperCase();
+    pHex.innerText = cor.hex.toUpperCase();
+    pNome.classList.add('nome');
+    pHex.classList.add('hex');
+
+    let corTexto = setCorTexto(cor.hex);
+    pNome.style.color = corTexto; 
+    pHex.style.color = corTexto; 
+
+    div.append(pNome, pHex);
+    return div;
+}
+
+function setCorTexto(cor) {
+    const r = parseInt(cor.slice(1, 3), 16);
+    const g = parseInt(cor.slice(3, 5), 16);
+    const b = parseInt(cor.slice(5, 7), 16);
+    const luminancia = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+    if (luminancia < 128) {
+        return 'var(--color6)';
+    } else {
+        return 'var(--color8)';
+    }
+}
